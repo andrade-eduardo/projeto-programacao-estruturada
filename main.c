60285450b68bfe30942e8d8dcf561c16704d3cf2
Eduardo Oliveira <oliveira.eduardo1104@gmail.com>
2025-11-05 11:20:20 -0300

    feat: add opcoes 3 e 4

--- main.c
+++ main.c
@@ -11,28 +11,29 @@ typedef struct produto {
 
 int main() {
 
+    // abrindo o arquivo produtos.txt
     FILE *arquivo = fopen("produtos.txt", "r");
     if(arquivo == NULL) {
         puts("Erro ao abrir arquivo\n");
         return 1;
     }
 
+    //salvando os produtos do arquivo no vetor produtos
     Produto *produtos = malloc(20 * sizeof(Produto));
     int qtd_produtos = 0;
     while(fscanf(arquivo, "%d %s %f %d", &produtos[qtd_produtos].codigo, produtos[qtd_produtos].nome, &produtos[qtd_produtos].preco, &produtos[qtd_produtos].quantidade) != EOF){
         qtd_produtos++;
     }
-    printf("%d\n", qtd_produtos);
     fclose(arquivo);
 
     int operacao;
     while(1) {
-        printf("\nDigite a operacao desejada:\n1. Adicionar produto\n2. Buscar produto por codigo\n3. Imprimir produtos\n4. Ordenar pro preco e imprimir\n5. Sair\n");
+        printf("\nDigite a operacao desejada:\n1. Adicionar produto\n2. Buscar produto por codigo\n3. Imprimir produtos\n4. Ordenar por preco e imprimir\n5. Sair\n");
         scanf("%d", &operacao);
         switch(operacao) {
             case 1:
                 produtos = realloc(produtos, qtd_produtos * sizeof(Produto) + sizeof(Produto));
-                printf("Digite o nome, preco e quantidade do produto\n");
+                printf("Digite o nome, preco e quantidade do produto:\n");
                 scanf("%s %f %d", produtos[qtd_produtos].nome, &produtos[qtd_produtos].preco, &produtos[qtd_produtos].quantidade);
                 produtos[qtd_produtos].codigo = produtos[qtd_produtos-1].codigo + 1;
                 qtd_produtos++;
@@ -50,22 +51,30 @@ int main() {
                 break;
             case 3:
                 for(int i = 0; i < qtd_produtos; i++) {
-                    printf("%d %s %.2f %d\n", produtos[i].nome, produtos[i].preco, produtos[i].quantidade);
+                    printf("%d %s %.2f %d\n", produtos[i].codigo, produtos[i].nome, produtos[i].preco, produtos[i].quantidade);
                 }
                 break;
             case 4:
+                for (int i = 0; i < qtd_produtos-1; i++) {
+                    for(int j = 0; j < qtd_produtos - i - 1; j++) {
+                        if(produtos[j].preco > produtos[j+1].preco) {
+                            Produto temp = produtos[j];
+                            produtos[j] = produtos[j+1];
+                            produtos[j+1] = temp;
+                        }
+                    }
+                }
+                for(int i = 0; i < qtd_produtos; i++) {
+                    printf("%d %s %.2f %d\n", produtos[i].codigo, produtos[i].nome, produtos[i].preco, produtos[i].quantidade);
+                }
                 break;
             case 5:
+                free(produtos);
                 return 0;
             default:
                 printf("Operacao invalida\n");
         }
     }
-
-    free(produtos);
-    /*for(int j = 0 ; j < i; j++){
-        printf("%d %s %f %d\n", produtos[j].codigo, produtos[j].nome, produtos[j].preco, produtos[j].quantidade);
-    }*/
 
     return 0;
 }
